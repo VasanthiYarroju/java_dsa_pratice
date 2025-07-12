b143c0214750f04e8f42712559ee79e4e339df6c
vasanthi <vasanthiyarroju@gmail.com>
2025-07-12 19:24:58 +0530

    Added Kadane’s Algorithm for max subarray

---
+++ week-1/arrays&strings/MaximumSubarraySum.java
@@ -0,0 +1,21 @@
+public class MaximumSubarraySum {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either extend the current subarray or start a new one
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = maxSubArray(nums);
+        System.out.println("Maximum subarray sum: " + result);
+    }
+}
