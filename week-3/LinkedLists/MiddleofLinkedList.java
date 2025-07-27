2af7997b3992ac6e6036f113a83dd9f3a22e53b8
vasanthi <vasanthiyarroju@gmail.com>
2025-07-27 21:44:36 +0530

    added a file

--- week-3/LinkedLists/MiddleofLinkedList.java
+++ week-3/LinkedLists/MiddleofLinkedList.java
@@ -16,7 +16,8 @@ public class MiddleofLinkedList {
         ListNode fast = head;
 
         while (fast != null && fast.next != null) {
-      
+            slow=slow.next;
+            fast=fast.next.next;
         }
 
         return slow; 
