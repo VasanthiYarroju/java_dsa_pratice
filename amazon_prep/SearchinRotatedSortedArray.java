641c57852fb3f725e4ac32fe02f6496bccdb0881
vasanthi <vasanthiyarroju@gmail.com>
2025-10-01 23:39:48 +0530

    6th  problem

--- amazon_prep/SearchinRotatedSortedArray.java
+++ amazon_prep/SearchinRotatedSortedArray.java
@@ -1,7 +1,7 @@
 public class SearchinRotatedSortedArray {
     public static void main(String[] args){
         int[] nums={4,5,6,7,0,1,2,3};
-        int target=5;
+        int target=0;
         System.out.println(search(nums,target));
     }
     public static int search(int[] nums, int target){
