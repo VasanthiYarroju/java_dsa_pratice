dad2bb9602391f3d799893ab832dcc3ce4c73f9b
vasanthi <vasanthiyarroju@gmail.com>
2025-11-11 11:51:08 +0530

    week-8 and week 9 for training

--- week-9/liking.java
+++ week-9/liking.java
@@ -1,23 +1,25 @@
 import java.util.Arrays;
+
 public class liking {
     public static void main(String[] args){
-        int[] l={-1,3,4};
+        int[] l = {-1, 3, 4};
         System.out.println(maxSatisfaction(l));
     }
+
     public static int maxSatisfaction(int[] l){
-        int n=l.length;
-        Arrays.sort(l);
-        int curr=0;
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            curr+=l[i];
-            if(curr>0){
-                ans+=curr;
+        int n = l.length;
+        Arrays.sort(l); 
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++){
+            int sum = 0;
+            int time = 1;
+            for (int j = i; j < n; j++){
+                sum += l[j] * time;
+                time++;
             }
-            else{
-                break;
-            }
+            max = Math.max(max, sum);
         }
-        return ans;
+        return max;
     }
 }
