4ea79bb1d4b0411a345e2918ead0e5d14fa8f53a
vasanthi <vasanthiyarroju@gmail.com>
2025-11-11 13:41:00 +0530

    week-8 and week 9 for training

--- week-9/series.java
+++ week-9/series.java
@@ -1,21 +1,25 @@
 public class series {
     public static void main(String[] args)
      {
-        int[] n = {1,4,16};
+        int[] n = {2,4,6,8,10};
         if(isArithmetic(n)) {
             System.out.println("The series is Arithmetic");
+            System.out.println("the next term is: " + (n[n.length-1] + (n[1]-n[0])));
         } else if(isGeometric(n)) {
             System.out.println("The series is Geometric");
+            System.out.println("the next term is: " + (n[n.length-1] * (n[1]/n[0])));
         } else if(isFibonacci(n)) {
             System.out.println("The series is Fibonacci");
+            System.out.println("the next term is: " + (n[n.length-1] + n[n.length-2]));
         }
         else {
-            System.out.println("The series is none of the above");
+            System.out.println("The series is neither Arithmetic nor Geometric nor Fibonacci");
+            System.out.println("999");
         }
     }
 
     public static boolean isArithmetic(int[] n){
-        int diff = n[1] - n[0];
+        int diff = n[1] -n[0];
         for(int i=2; i<n.length; i++){
             if(n[i] - n[i-1] != diff){
                 return false;
