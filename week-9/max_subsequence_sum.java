fbbed27a9d5a1fb3cec96b7e047ddb4f676e425b
vasanthi <vasanthiyarroju@gmail.com>
2025-11-11 15:06:21 +0530

    week-8 and week 9 for training

--- week-9/max_subsequence_sum.java
+++ week-9/max_subsequence_sum.java
@@ -1,14 +1,19 @@
 public class max_subsequence_sum {
     public static void main(String[] args) {
-        int[] arr = {-1,2,-1,4};
+        int[] arr = {1,2,4,-2,3};
         System.out.println(maxSubArray(arr));
     }
     public static int maxSubArray(int[] arr){
         int curr=arr[0];
         int max=arr[0];
         for(int i=1;i<arr.length;i++){
-            curr=Math.max(arr[i],curr+arr[i]);
-            max=Math.max(max,curr);
+           curr=curr+arr[i];
+           if(curr>max){
+                max=curr;
+           }
+           if(arr[i]<0){
+                curr=0;
+           }
         }
         return max;
     }
