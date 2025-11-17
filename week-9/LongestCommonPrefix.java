6cdd8450cf5909a5d04cf650b893b03d63f61f2b
vasanthi <vasanthiyarroju@gmail.com>
2025-11-17 21:46:56 +0530

    week-8 and week 9 for training

--- week-9/LongestCommonPrefix.java
+++ week-9/LongestCommonPrefix.java
@@ -21,4 +21,23 @@ public class LongestCommonPrefix{
         String[] strs={"flower","flow","flight"};
         System.out.println(longestcommonprefix(strs));
     }
+
+    //now we will implement the vertical scanning approach
+    /*
+    public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0) return "";
+
+    String prefix = strs[0]; // assume first string is the prefix
+
+    for (int i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) != 0) { 
+            // shorten prefix until it matches
+            prefix = prefix.substring(0, prefix.length() - 1);
+            if (prefix.isEmpty()) return "";
+        }
+    }
+    return prefix;
+}
+
+     */
 }
